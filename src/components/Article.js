b784ffa8158d2a965174c86f6a76c8f466398f07
deenoloo <doreenoloo2@gmail.com>
2025-04-09 08:13:39 -0400

    first changes

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,8 +1,14 @@
-function Article ({title, date= "January 1, 1970", preview}){
+function Article ({title, date= "January 1, 1970", preview,minutes}){
+
+    const emoji = minutes < 30 
+    ? '☕️'.repeat(Math.ceil(minutes / 5)) 
+    : '🍱'.repeat(Math.ceil(minutes / 10));
+  
+  const readingTime = `${emoji} ${minutes} min read`;
     return (
         <article>
         <h3>{title}</h3>
-        <small>{date}</small>
+        <small>{date}.{readingTime}</small>
         <p>{preview}</p>
     </article>
     )   
